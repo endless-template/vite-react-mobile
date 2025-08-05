c81478c36bbd89d220dcb16c4a175e5f6c055420
collins401 <xiefeitai@163.com>
2025-08-05 21:26:12 +0800

    fix: 文案修改

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,14 @@ function Home() {
       <NavBar back={null}>首页</NavBar>
       <div className="my-4 rounded bg-white py-4">
         <List header="🎉功能特性">
-          <List.Item>👉Vite5.x + TypeScript 构建移动端基础脚手架</List.Item>
+          <List.Item>👉Vite7.x + TypeScript 构建移动端基础脚手架</List.Item>
           <List.Item>👉React19 + React Compiler</List.Item>
           <List.Item>👉基于Antd-Mobile 自定义Design System</List.Item>
           <List.Item onClick={() => go('/ofetch')}>👉ofetch封装及接口管理</List.Item>
           <List.Item>👉Tailwindcss + rem适配</List.Item>
           <List.Item>👉Eslint + Prettier + CommitLint 规范</List.Item>
           <List.Item>👉集成svg图标组件</List.Item>
-          <List.Item>👉兼容 Android 6.0+, iOS10.3+</List.Item>
+          <List.Item>👉兼容 Android 7.0+, iOS11+</List.Item>
         </List>
         <div className="ml-3 pt-3 text-base text-color/60">🎉enjoy ~</div>
       </div>
